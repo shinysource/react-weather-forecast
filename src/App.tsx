36278d0e7063ca1ddcb84203d5c8271d7d14c40a
shinysource <highly.flores@gmail.com>
2022-11-13 20:59:19 -0500

    Build directory structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.scss";
+import React, { createContext } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-function App() {
+import Layout from "./layouts";
+import Main from "./pages/Main";
+
+export const WeatherContext = createContext({
+  weatherState: [],
+  setWeatherState: () => {},
+});
+
+const App: React.FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WeatherContext.Provider
+      value={{ weatherState: [], setWeatherState: () => [] }}
+    >
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </Layout>
+        <ToastContainer />
+      </Router>
+    </WeatherContext.Provider>
   );
-}
+};
 
 export default App;
